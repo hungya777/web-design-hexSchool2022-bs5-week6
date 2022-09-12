36be37e8bbf05084be08c0aa7423472890869162
hungya <hungya777@gmail.com>
2022-09-12 22:18:52 +0800

    first commit

---
+++ app/assets/js/vendors.js
@@ -0,0 +1,53 @@
+$(function() {
+  const swiperDefault = new Swiper(".swiper-default", {
+    slidesPerView: 3,
+    spaceBetween: 16,
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+  });
+  const swiper = new Swiper(".swiper-auto", {
+    slidesPerView: 4,
+    spaceBetween: 8,
+    //多欄
+    breakpoints: {
+        768: {
+            slidesPerView: 6,
+            spaceBetween: 8,
+        },
+        992: {
+            slidesPerView: 9,
+            spaceBetween: 16,
+            
+        }
+    },
+    speed: 2000,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 0
+    },
+  });
+
+  //Effect fade
+  const swiperFade = new Swiper(".swiper-fade", {
+    // Optional parameters
+    effect: "fade",
+    loop: true,
+    fadeEffect: {
+      crossFade: true
+    },
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    // 左右箭頭
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+});
